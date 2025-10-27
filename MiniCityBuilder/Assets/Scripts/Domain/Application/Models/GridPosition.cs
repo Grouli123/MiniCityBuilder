6154b3310a123ebebed99bc7ad34865ed93694fb
Grouli123 <Grouli123@yandex.ru>
2025-10-27 15:41:33 +0300

    feature(domain): add GridPosition value type

---
+++ MiniCityBuilder/Assets/Scripts/Domain/Application/Models/GridPosition.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Application.Domain.Application.Models
+{
+    /// <summary>Координата клетки сетки (целочисленная).</summary>
+    public readonly struct GridPosition
+    {
+        public readonly int X;
+        public readonly int Y;
+
+        public GridPosition(int x, int y) { X = x; Y = y; }
+
+        public override string ToString() => $"({X},{Y})";
+        public override int GetHashCode() => (X * 397) ^ Y;
+        public override bool Equals(object obj) => obj is GridPosition p && p.X == X && p.Y == Y;
+        public static bool operator ==(GridPosition a, GridPosition b) => a.Equals(b);
+        public static bool operator !=(GridPosition a, GridPosition b) => !a.Equals(b);
+    }
+}
